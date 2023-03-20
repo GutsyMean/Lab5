ecda9e964d40e846c6cb044d1e7d5d0278d2df97
GutsyMean <timothy.bogun@gmail.com>
2023-03-19 22:01:39 -0400

    commit 2

--- src/advancedGUI/BookStoreGUI.java
+++ src/advancedGUI/BookStoreGUI.java
@@ -1,10 +1,26 @@
 package advancedGUI;
 
-public class BookStoreGUI {
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+import javafx.event.*;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class BookStoreGUI extends Application
+{
 
+	public static void main(String[] args) 
+	{
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage)
+	{
+		
 	}
 
 }
